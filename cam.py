c6e4f02857559b3211b5f661443ab5d8a49f8502
Marc Soler <m.e.soler@me.com>
2022-10-17 14:33:57 +0200

    Add export delay (mitigates blurry images)

--- cam.py
+++ cam.py
@@ -4,32 +4,53 @@ import signal
 import cv2
 import numpy as np
 
-class Camera():
+
+class Camera:
     """
     Webcam access from: https://stackoverflow.com/questions/604749/how-do-i-access-my-webcam-in-python
     Movement detection from: http://www.robindavid.fr/opencv-tutorial/motion-detection-with-opencv.html
     """
 
-    def __init__(self, device=0, preview=False, threshold=8):
+    def __init__(self, device=0, preview=False, threshold=8, export_path="snap.png"):
         self.preview = preview
         self.vc = cv2.VideoCapture(device)
-        _, self.frame = self.vc.read()
-        self.height, self.width, _ = self.frame.shape
+        _, frame = self.vc.read()
+        self.height, self.width, _ = frame.shape
         self.nb_pixels = self.width * self.height
 
+        # Movement parameters
         self.min_threshold = (self.nb_pixels / 100) * threshold
+        self.detection_time = 0
+        self.movement_threshold = 5  # number of frames before movement alert is triggered
+        self.movement_counter = 0
+        self.export_delay = 10  # the delay in frames after which the image is exported (to mitigate blurred images)
+        self.export_delay_counter = 0
+        self.export_queued = False
+
+        signal.signal(signal.SIGINT, self.close)
+        self.export_path = export_path
+
+        # Initialise data structures
+        self.initialise_setup()
+        _, frame = self.vc.read()
         # Main results matrix
         self.res = np.zeros([self.height, self.width], dtype="uint8")
         # Make grayscale frame at t-1
         self.frame1gray = np.zeros([self.height, self.width], dtype="uint8")
-        self.frame1gray = cv2.cvtColor(self.frame, cv2.COLOR_RGB2GRAY)
+        self.frame1gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         # Make empty frame at t
         self.frame2gray = np.zeros([self.height, self.width], dtype="uint8")
-        # Last detection
-        self.detection_time = 0
-        self.movement_threshold = 20 # number of frames before movement alert is triggered
-        self.movement_counter = 0
-        signal.signal(signal.SIGINT, self.close)
+
+    def initialise_setup(self):
+        # To have fresh data in the structures
+        _, frame = self.vc.read()
+        # Main results matrix
+        self.res = np.zeros([self.height, self.width], dtype="uint8")
+        # Make grayscale frame at t-1
+        self.frame1gray = np.zeros([self.height, self.width], dtype="uint8")
+        self.frame1gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+        # Make empty frame at t
+        self.frame2gray = np.zeros([self.height, self.width], dtype="uint8")
 
     def run(self):
         if self.preview:
@@ -52,16 +73,25 @@ class Camera():
                 cv2.imshow("Preview", frame)
             rval, frame = self.vc.read()
             instant = time.time()
-            self.processImage(frame)
+            self.process_image(frame)
 
             if self.movement_detected():
                 self.detection_time = instant
                 print(f"Movement detected at {self.detection_time}")
+                self.export_queued = True
+
+            if self.export_queued:
+                if self.export_delay_counter < self.export_delay:
+                    self.export_delay_counter += 1
+                else:
+                    self.export_delay_counter = 0
+                    self.export_queued = False
+                    self.export_image(frame)
+                    break
             self.frame1gray = self.frame2gray
 
-
     def export_image(self, frame):
-        pass
+        cv2.imwrite(self.export_path, frame)
 
     def close(self, signum, frame):
         print("Closing camera")
@@ -82,7 +112,7 @@ class Camera():
             self.movement_counter = 0
             return False
 
-    def processImage(self, frame):
+    def process_image(self, frame):
         # Converts image to grayscale
         self.frame2gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
         # Compare difference between frames and store into res
