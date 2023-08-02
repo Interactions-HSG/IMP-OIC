9379db77f76eb5d0e29123118ce0041742d2ffb8
Sanjiv Jha <jhasanjiv5@gmail.com>
2023-08-02 21:08:07 +0200

    Refactor: question selection and oic resp update

---
+++ STAR_eval/gpt_ask.py
@@ -0,0 +1,19 @@
+import sys
+import openai
+import configparser
+config = configparser.ConfigParser()
+config.read('config.ini')
+openai.organization = "org-bOaL53AMHfnPzifP0AcjW2Fg"
+openai.api_key = config['DEFAULT']['open_ai_api_key']
+models = openai.Model.list()
+
+def run_gpt(context, question):
+    #with open(context) as f:
+    #    context_lines = "".join(map(str,f.readlines())) 
+    response = openai.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "system", "content": context},
+                            {"role": "user", "content": question}
+                ])
+
+    return response.choices[0].message.content
